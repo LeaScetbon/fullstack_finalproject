0a5dcc2172673eba49e0a6618eb98f4ae6f56f84
LeaScetbon <73163251+LeaScetbon@users.noreply.github.com>
2023-07-31 14:05:28 +0300

    trying payment

--- src/pages/PaymentDetails.js
+++ src/pages/PaymentDetails.js
@@ -1,126 +1,116 @@
-import React, { useState, useEffect } from 'react';
-
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 function PaymentDetails() {
-    const [formData, setFormData] = useState({
-        name: '',
-        identityCard: '',
-        cardNumber: '',
-        expirationDate: '',
-        cvv: '',
+  const [formData, setFormData] = useState({
+    name: '',
+    cardNumber: '',
+    expirationDate: '',
+    cvv: '',
+  });
+  const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const userId = JSON.parse(localStorage.getItem('username')).id;
+      if (!userId) {
+        alert('User ID not found in local storage');
+        return;
+      }
+  
+      // Convert cardNumber to a number before sending to the server
+      const updatedFormData = {
+        ...formData,
+        cardNumber: Number(formData.cardNumber),
+        username: JSON.parse(localStorage.getItem('username')).username,
+      };
+  
+      const response = await fetch(`http://localhost:3001/users/${userId}/MyCart/PaymentDetails`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(updatedFormData),
       });
-    
-      const handleChange = (e) => {
-        const { name, value } = e.target;
-        setFormData({ ...formData, [name]: value });
-      };
-    
-      const handleSubmit = (e) => {
-        // e.preventDefault();
-        //    try {
-        //    const response = await fetch("http://localhost:3001/PaymentDetails", {
-        //      method: "POST",
-        //      headers: {
-        //        "Content-Type": "application/json",
-        //      },
-        //      body: JSON.stringify({ username, email, userpassword }),
-        //    });
-         
-      };
-      // const handleRegister = async (e) => {
-      //   e.preventDefault();
-    
-      //   try {
-      //     const response = await fetch("http://localhost:3001/register", {
-      //       method: "POST",
-      //       headers: {
-      //         "Content-Type": "application/json",
-      //       },
-      //       body: JSON.stringify({ username, email, userpassword }),
-      //     });
-    
-      //     if (response.ok) {
-      //       alert("Registration successful! Please login with your new account.");
-      //       // Clear the input fields after successful registration
-      //       setUsername("");
-      //       setUserPassword("");
-      //       setEmail("");
-      //       navigate("/login");
-      //     } else {
-      //       const data = await response.json();
-      //       alert(data.error);
-      //     }
-      //   } catch (error) {
-      //     console.error("Error during registration:", error);
-      //     alert("An error occurred during registration");
-      //   }
-      // };
-    
-    
-      return (
+  
+      const data = await response.json();
+      console.log(data);
+      // Assuming the response from the server contains a "message" field for successful payment
+      if (response.ok) {
+        alert(data.message);
+        navigate("/PaymentConfirmation");
+      } else {
+        alert(data.error);
+      }
+    } catch (error) {
+      console.error(error);
+      alert('An error occurred while processing the payment');
+    }
+  };
+  
+
+  return (
+    <div>
+      <h2>Bank Account Details</h2>
+      <form onSubmit={handleSubmit}>
         <div>
-          <h2>Bank Account Details</h2>
-          <form onSubmit={handleSubmit}>
-            <div>
-              <label htmlFor='name'>Name:</label>
-              <input
-                type='text'
-                id='name'
-                name='name'
-                value={formData.name}
-                onChange={handleChange}
-                required
-              />
-            </div>
-            <div>
-              <label htmlFor='identityCard'>Identity Card:</label>
-              <input
-                type='text'
-                id='identityCard'
-                name='identityCard'
-                value={formData.identityCard}
-                onChange={handleChange}
-                required
-              />
-            </div>
-            <div>
-              <label htmlFor='cardNumber'>Card Number:</label>
-              <input
-                type='text'
-                id='cardNumber'
-                name='cardNumber'
-                value={formData.cardNumber}
-                onChange={handleChange}
-                required
-              />
-            </div>
-            <div>
-              <label htmlFor='expirationDate'>Expiration Date:</label>
-              <input
-                type='text'
-                id='expirationDate'
-                name='expirationDate'
-                value={formData.expirationDate}
-                onChange={handleChange}
-                required
-              />
-            </div>
-            <div>
-              <label htmlFor='cvv'>CVV:</label>
-              <input
-                type='text'
-                id='cvv'
-                name='cvv'
-                value={formData.cvv}
-                onChange={handleChange}
-                required
-              />
-            </div>
-            <button type='submit'>Pay Now</button>
-          </form>
+          <label htmlFor="name">Name:</label>
+          <input
+            type="text"
+            id="name"
+            name="name"
+            value={formData.name}
+            onChange={handleChange}
+            required
+          />
         </div>
-      );
- 
+    
+        <div>
+          <label htmlFor="cardNumber">Card Number:</label>
+          <input
+            type="text"
+            id="cardNumber"
+            name="cardNumber"
+            value={formData.cardNumber}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <div>
+          <label htmlFor="expirationDate">Expiration Date:</label>
+          <input
+            type="date"
+            id="expirationDate"
+            name="expirationDate"
+            value={formData.expirationDate}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <div>
+          <label htmlFor="cvv">CVV:</label>
+          <input
+            type="text"
+            id="cvv"
+            name="cvv"
+            value={formData.cvv}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <button type="submit">Pay Now</button>
+      </form>
+    </div>
+  );
 }
 
 export default PaymentDetails;
+
+
+
--- src/server/PaymentDetailsServer.js
+++ src/server/PaymentDetailsServer.js
@@ -12,7 +12,46 @@ router.use((req, res, next) => {
   next();
 
   
+
 });
+
+router.post('/users/:userId/MyCart/PaymentDetails', async (req, res) => {
+  const { username, cardNumber, expirationDate, cvv } = req.body;
+
+  if (!username || !cardNumber || !expirationDate || !cvv) {
+    return res.status(400).json({ error: 'Please fill all fields' });
+  }
+
+  const query = 'SELECT * FROM users WHERE username = ?';
+
+  connection.query(query, [username], (error, results) => {
+    if (error) {
+      console.error('Error executing the query: ', error);
+      res.status(500).json({ error: 'Internal Server Error' });
+    } else {
+      if (results.length === 0) {
+        return res.status(404).json({ error: 'User not found' });
+      }
+
+      const user = results[0];
+
+      // Check if the user's card details match the form data
+      if (
+        user.card_number === cardNumber &&
+        user.expiration_date === expirationDate &&
+        user.cvv === cvv
+      ) {
+        return res.status(200).json({ message: 'Payment successful!' });
+      } else {
+        return res.status(400).json({ error: 'Invalid payment details' });
+      }
+    }
+  });
+});
+
+
+
+
 
 
 
